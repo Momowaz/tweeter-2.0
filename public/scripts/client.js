3777a8b9c58b9aef887afd52d6f70b6facc87135
MohibHome <mohib@can-health.org>
2023-05-09 21:31:35 -0700

    Add form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,6 @@
  */
 $(document).ready(function () {
 
-    
-    const data = [
-        {
-            user: {
-                name: "Newton",
-                avatars: "https://i.imgur.com/73hZDYK.png",
-                handle: "@SirIsaac",
-            },
-            content: {
-                text: "If I have seen further it is by standing on the shoulders of giants",
-            },
-            created_at: 1461116232227,
-        },
-        {
-            user: {
-                name: "Descartes",
-                avatars: "https://i.imgur.com/nlhLi3I.png",
-                handle: "@rd",
-            },
-            content: {
-                text: "Je pense , donc je suis",
-            },
-            created_at: 1461113959088,
-        },
-    ];
-
     const renderTweets = function (tweets) {
         // loops through tweets
         // calls createTweetElement for each tweet
@@ -39,42 +13,70 @@ $(document).ready(function () {
         for (const tweet of tweets) {
             const $tweet = createTweetElement(tweet);
             $tweetContainer.append($tweet);
+            $tweet.find('.timeago').timeago("update", new Date());
         }
     };
 
     const createTweetElement = function (tweet) {
         const $tweet = $(`
-      <article class="tweet">
-            <header class="tweet-header">
-              <span><img src="${tweet.user.avatars}" ${tweet.user.name}</span>
-              <span>${tweet.user.handle}</span>
-            </header>
-            <p class="msg">
-              ${tweet.content.text}
-            </p>
-            <footer class="tweet-footer">
-                100 days ago
-              <span class="icons">
-                <i class="fa-solid fa-flag"></i>
-                <i class="fa-solid fa-repeat"></i>
-                <i class="fa-solid fa-heart"></i>
-              </span>
-            </footer>
-          </article>`);
-
+            <article class="tweet">
+                <header class="tweet-header">
+                    <span><img src="${tweet.user.avatars}" ${tweet.user.name}</span>
+                    <span>${tweet.user.handle}</span>
+                </header>
+                <p class="msg">
+                    ${tweet.content.text}
+                </p>
+                <footer class="tweet-footer" data-created-at="${tweet.created_at}">
+                    <span class="timeago"></span>
+                    <span class="icons">
+                        <i class="fa-solid fa-flag"></i>
+                        <i class="fa-solid fa-repeat"></i>
+                        <i class="fa-solid fa-heart"></i>
+                    </span>
+                </footer>
+            </article>`);
+    
         return $tweet;
     };
+    
 
     $("#form").on("submit", function(event) {
         // alert('submitted');
         
         event.preventDefault();
-        const tweetData = $( this ).serialize();
-        $.post('/tweets', {text: tweetData}) 
+        
+        const $form = $(this);
+        const $tweetContent = $form.find('textarea[name="tweet"]');
+        const tweetContent = $("#tweet-text").val();
+
+        if (!tweetContent) {
+            alert('Error: Tweet content is empty');
+            return;
+        }
+        if (tweetContent.length > 140) {
+            alert('Error: Tweet content exceeds 140 characters');
+            return;
+        }
+        
+        $.post('/tweets', $form.serialize()) 
         .done( (response) => { 
             console.log(response);
         })
-        
+        $tweetContent.val("");
     });
+
+    const loadTweets = function() {
+        $.getJSON('/tweets', function(data) {
+          // success callback function
+          console.log('Received JSON data:', data);
+          renderTweets(data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+          // error callback function
+          console.error('Error loading tweets:', textStatus, errorThrown);
+        });
+      };
+      
+    loadTweets();
     renderTweets(data);
 });
